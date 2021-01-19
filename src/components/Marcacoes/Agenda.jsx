c3b95fe97616684003abc547c3b60f590ec67241
Antonio <tofernandes67@gmail.com>
2021-01-19 17:07:34 +0000

    basic agenda working

--- src/components/Marcacoes/Agenda.jsx
+++ src/components/Marcacoes/Agenda.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState,} from 'react'
 import { ReactAgenda , ReactAgendaCtrl , guid ,  Modal } from 'react-agenda';
 import _ from 'lodash';
-
+require('moment/locale/pt.js');
 var colors= {
   'color-1':"rgba(102, 195, 131 , 1)" ,
   "color-2":"rgba(242, 177, 52, 1)" ,
@@ -43,47 +43,90 @@ export default function Agenda() {
       name          : 'Working lunch , Holly',
       startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 11, 0),
       endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 13, 0),
-      classes       : 'color-2 color-3'
+      classes       : 'color-2'
     },
   
   ])
+  const [showModal, setModal] = useState(false)
+  const [selected, setSelected] = useState();
   useEffect(() => {
     if(!_.isEqual(JSON.stringify(newItems), JSON.stringify(items)) ){
-      console.log('hey')
       setItems(newItems);
     }
   }, [items,newItems,setItems])
 
 
-  const handleCellSelection = (item) =>{
-    console.log('handleCellSelection',item)
+  const handleCellSelection = () =>(e) =>{
+    
+    if(selected  && selected[0] === e ){
+      setModal(true)
+    }
+    setSelected([e]);
   }
-  const handleItemEdit= (item) =>{
-    console.log('handleItemEdit', item)
+
+  const handleRangeSelection = () =>(e) =>{
+    setModal(true)
+    setSelected(e);
   }
-  const handleRangeSelection= (item) =>{
-    console.log('handleRangeSelection', item)
+
+  const editEvent = (items) => (e) =>{
+    console.log(items)
+
+    setModal(false);
+    setSelected([]);
+    setNew(items)
+    closeModal();
   }
+
+  const handleItemEdit= () =>(e) =>{
+    if(e){
+      setSelected([e])
+      return openModal();
+    }
+  }
+ 
 
   const mudar= (itens) => (e) => {
     //console.log(JSON.parse(JSON.stringify(itens)))
    setNew(JSON.parse(JSON.stringify(itens)))
   }
+
+  const closeModal = () =>(e) => {
+    if(e){
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    setModal(false);
+  }
+
+ const addNewEvent = (items , newItems) => (e)=>{
+    setModal(false);
+    setSelected([])
+    setNew(e);
+    console.log(e)
+    closeModal();
+  }
+  
+
+  const openModal = () => {
+    setModal(true);
+  }
   
   return (
+    <>
     <div className="page-content-wrapper">
       <div className="page-content">
         <div className="page-bar">
           <div className="page-title-breadcrumb">
             <div className=" pull-left">
-              <div className="page-title">Calendar</div>
+              <div className="page-title">Agenda</div>
             </div>
             <ol className="breadcrumb page-breadcrumb pull-right">
               <li><i className="fa fa-home"></i>&nbsp;<a className="parent-item" href="index.html">Home</a>&nbsp;<i className="fa fa-angle-right"></i>
               </li>
               <li><a className="parent-item" href="">Apps</a>&nbsp;<i className="fa fa-angle-right"></i>
               </li>
-              <li className="active">Calendar</li>
+              <li className="active">Agenda</li>
             </ol>
           </div>
         </div>
@@ -91,7 +134,7 @@ export default function Agenda() {
           <div className="col-md-9 col-sm-12">
             <div className="card-box">
               <div className="card-head">
-                <header>Calendar</header>
+                <header>Agenda</header>
               </div>
               <div className="card-body ">
                 <div className="panel-body">
@@ -100,49 +143,36 @@ export default function Agenda() {
           maxDate={new Date(now.getFullYear(), now.getMonth()+3)}
           disablePrevButton={false}
           startDate={new Date()}
-          cellHeight={30}
-          locale={'pt'}
+          cellHeight={60}
+          locale={'pt_br'}
           items={items}
-          numberOfDays={4}
+          numberOfDays={5}
           rowsPerHour={2}
           itemColors={colors}
           autoScale={false}
           fixedHeader={true}
-          onItemEdit={() => handleItemEdit(items)}
-          onCellSelect={() => handleCellSelection(items)}
-          onRangeSelection={()=>handleRangeSelection(items)}
+          onItemEdit={ handleItemEdit()}
+          onCellSelect={handleCellSelection()}
+          onRangeSelection={handleRangeSelection()}
           onChangeEvent	= {mudar(items)}
-          onItemEdit = {() => console.log('edit')}
           />
                 </div>
               </div>
             </div>
           </div>
-          <div className="col-md-3 col-sm-12">
-            <div className="card-box">
-              <div className="card-head">
-                <header>Draggable Events</header>
-              </div>
-              <div className="card-body ">
-                <div id="external-events">
-                  <form className="inline-form">
-                    <input type="text" value="" className="form-control" placeholder="Event Title..." id="event_title" />
-                    <br />
-                    <a href="javascript:;" id="event_add" className="btn deepPink-bgcolor"> Add Event </a>
-                  </form>
-                  <hr />
-                  <div id="event_box" className="mg-bottom-10"></div>
-                  <label className="rt-chkbox rt-chkbox-single rt-chkbox-outline" for="drop-remove"> remove after drop
-                                    <input type="checkbox" className="group-checkable" id="drop-remove" />
-                    <span></span>
-                  </label>
-                  <hr className="visible-xs" />
-                </div>
-              </div>
-            </div>
-          </div>
+
+          
         </div>
       </div>
     </div>
+    {
+      showModal? <Modal clickOutside={closeModal} >
+          <div className="modal-content">
+             <ReactAgendaCtrl items={items} itemColors={colors}  selectedCells={selected} edit={editEvent()} Addnew={addNewEvent()} />
+
+          </div>
+   </Modal>:''
+}
+   </>
   )
 }
